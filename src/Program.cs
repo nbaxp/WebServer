2008ae4643cb9f8beae26f45ecb8e30bf9b27b8e
wanggang <76527413@qq.com>
2021-10-26 17:26:29 +0800

    update

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.IO;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 
@@ -20,64 +21,68 @@ namespace WebServer
         public static void Main(string[] args)
         {
             Host.CreateDefaultBuilder(args)
-               .ConfigureWebHostDefaults(webBuilder =>
-               {
-                   webBuilder.ConfigureLogging(o =>
-                   {
-                       o.ClearProviders();
-                       o.AddConsole();
-                   });
-                   webBuilder.ConfigureServices(services =>
-                   {
-                       services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.All));
-                       services.AddHttpClient();
-                       services.AddCors(options => options.AddPolicy(_origins,
-                           builder =>
-                           {
-                               builder.SetIsOriginAllowed(o => true)
-                               .AllowAnyMethod()
-                               .AllowAnyHeader()
-                               .AllowCredentials();
-                           }));
-                       services.AddControllersWithViews();
-                       using var o = services.BuildServiceProvider();
-                       var config = o.GetService<IConfiguration>();
-                       if (config.GetValue("spa", true))
-                       {
-                           services.AddSpaStaticFiles(configuration =>
-                           {
-                               configuration.RootPath = config.GetValue("wwwroot", "wwwroot");
-                           });
-                       }
-                   });
-                   webBuilder.Configure(app =>
-                   {
-                       app.UseCors(_origins);
-                       app.UseHttpsRedirection();
-                       app.UseStaticFiles();
-                       app.UseFileServer();
-                       app.UseRouting();
-                       app.UseAuthorization();
-                       app.UseEndpoints(endpoints =>
-                       {
-                           endpoints.MapControllerRoute(
-                               name: "default",
-                               pattern: "{controller}/{action=Index}/{id?}");
-                       });
-                       var config = app.ApplicationServices.GetService<IConfiguration>();
-                       if (config.GetValue("spa", true))
-                       {
-                           app.UseSpaStaticFiles();
-                           app.UseSpa(spa =>
-                           {
-                               spa.Options.SourcePath = config.GetValue("wwwroot", "wwwroot");
-                           });
-                       }
-                       app.UseDefaultFiles();
-                   });
-               })
-               .Build()
-               .Run(); ;
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true)
+                    .Build();
+                    webBuilder.UseConfiguration(config);
+                    webBuilder.ConfigureLogging(o =>
+                    {
+                        o.ClearProviders();
+                        o.AddConsole();
+                    });
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.All));
+                        services.AddHttpClient();
+                        services.AddCors(options => options.AddPolicy(_origins,
+                            builder =>
+                            {
+                                builder.SetIsOriginAllowed(o => true)
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .AllowCredentials();
+                            }));
+                        services.AddControllersWithViews();
+                        using var o = services.BuildServiceProvider();
+                        var config = o.GetService<IConfiguration>();
+                        if (config.GetValue("spa", true))
+                        {
+                            services.AddSpaStaticFiles(configuration =>
+                            {
+                                configuration.RootPath = config.GetValue("wwwroot", "wwwroot");
+                            });
+                        }
+                    });
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseCors(_origins);
+                        app.UseHttpsRedirection();
+                        app.UseStaticFiles();
+                        app.UseFileServer();
+                        app.UseRouting();
+                        app.UseAuthorization();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllerRoute(
+                                name: "default",
+                                pattern: "{controller}/{action=Index}/{id?}");
+                        });
+                        var config = app.ApplicationServices.GetService<IConfiguration>();
+                        if (config.GetValue("spa", true))
+                        {
+                            app.UseSpaStaticFiles();
+                            app.UseSpa(spa =>
+                            {
+                                spa.Options.SourcePath = config.GetValue("wwwroot", "wwwroot");
+                            });
+                        }
+                        app.UseDefaultFiles();
+                    });
+                })
+                .Build()
+                .Run(); ;
         }
     }
 }
