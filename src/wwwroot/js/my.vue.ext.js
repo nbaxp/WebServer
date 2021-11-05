72086223202a86ec5130d3728b3b370ff8ae507e
wanggang <76527413@qq.com>
2021-11-05 13:36:38 +0800

    修复sfc中import的路径问题

--- src/wwwroot/js/my.vue.ext.js
+++ src/wwwroot/js/my.vue.ext.js
@@ -1,13 +1,19 @@
 var MyVueExt = (function () {
     var exports = {
         debug: false,
-        componentsPath: '/components',
-        viewsPath:'/views',
-        componentExt: '.html',
-        styleCounter: 'component-style-counter',
-        routerHome: '/home'
+        componentsPath: "/components",
+        viewsPath: "/views",
+        componentExt: ".html",
+        styleCounter: "component-style-counter",
+        routerHome: "/home",
     };
-    exports.basePath = trimEnd(document.location.protocol + '//' + document.location.host + document.querySelector('base')?.getAttribute('href') ?? document.location.href);
+    var basePath =
+        document.location.protocol +
+        "//" +
+        document.location.host +
+        document.querySelector("base")?.getAttribute("href") ??
+        document.location.href;
+    exports.basePath = trimEnd(basePath, "/");
     function log(msg) {
         if (exports.debug) {
             console.log(msg);
@@ -18,89 +24,107 @@ var MyVueExt = (function () {
     }
     function append(parent, html) {
         var frag = document.createDocumentFragment();
-        var div = document.createElement('div');
+        var div = document.createElement("div");
         div.innerHTML = html;
-        var list = div.querySelectorAll('*');
+        var list = div.querySelectorAll("*");
         for (var i = 0; i < list.length; i++) {
             parent.appendChild(list[i]);
         }
     }
     function addStyles(name, style) {
-        var styleList = document.querySelectorAll('head style.' + name);
+        var styleList = document.querySelectorAll("head style." + name);
         if (styleList.length > 0) {
             for (var i = 0; i < styleList.length; i++) {
                 var style = styleList[i];
-                var counter = parseInt(style.getAttribute(exports.styleCounter));
+                var counter = parseInt(
+                    style.getAttribute(exports.styleCounter)
+                );
                 style.setAttribute(exports.styleCounter, counter + 1);
             }
-        }
-        else {
+        } else {
             if (style) {
-                append(document.head, style)
+                append(document.head, style);
             }
         }
     }
     function removeStyles(name) {
-        var styleList = document.querySelectorAll('head style.' + name);
+        var styleList = document.querySelectorAll("head style." + name);
         if (styleList.length > 0) {
             for (var i = 0; i < styleList.length; i++) {
                 var style = styleList[i];
-                var counter = parseInt(style.getAttribute(exports.styleCounter));
+                var counter = parseInt(
+                    style.getAttribute(exports.styleCounter)
+                );
                 if (counter - 1 > 0) {
                     style.setAttribute(exports.styleCounter, counter - 1);
-                }
-                else {
+                } else {
                     document.head.removeChild(style);
                 }
             }
         }
     }
-    function templateToModel(html, name, url) {
+    function templateToModel(html, name, url, func) {
         log({ url });
-        var doc = new DOMParser().parseFromString(html, 'text/html');
-        var templateTag = doc.querySelector('template');
-        var template = templateTag ? templateTag.innerHTML : '<template></template>';
-        var scriptTag = doc.querySelector('script');
-        var script = scriptTag ? scriptTag.innerHTML : '{}';
-        var modelScript = '(' + (script.replace(/^\s*export\s*default\s*/, '').replace(/;?\s*$/, '') || '{}') + ')\n//@ sourceURL=' + url;
-        var model = eval(modelScript);
-        var styleTagList = doc.querySelectorAll('style');
-        if (styleTagList.length > 0) {
-            model.style = '\n';
-            for (var i = 0; i < styleTagList.length; i++) {
-                var styleTag = styleTagList[i];
-                if (styleTag) {
-                    styleTag.setAttribute('class', name);
-                    styleTag.setAttribute(exports.styleCounter, 1);
-                    model.style += styleTag.outerHTML + '\n';
+        var doc = new DOMParser().parseFromString(html, "text/html");
+        var templateTag = doc.querySelector("template");
+        var template = templateTag ? templateTag.innerHTML : "<template></template>";
+        var scriptTag = doc.querySelector("script");
+        var script = scriptTag?.innerHTML || '';
+        evalByImportUriData(script, url, function (model) {
+            model.template = template;
+            var styleTagList = doc.querySelectorAll("style");
+            if (styleTagList.length > 0) {
+                model.style = "\n";
+                for (var i = 0; i < styleTagList.length; i++) {
+                    var styleTag = styleTagList[i];
+                    if (styleTag) {
+                        styleTag.setAttribute("class", name);
+                        styleTag.setAttribute(exports.styleCounter, 1);
+                        model.style += styleTag.outerHTML + "\n";
+                    }
                 }
             }
-        }
-        model.template = template;
-        return model;
+            func(model);
+        });
     }
     function addComponent(instance, name, url) {
-        instance.component(name, exports.defineAsyncComponent(() => new Promise((resolve, reject) => {
-            fetch(url).then(function (response) {
-                return response.text();
-            }).then(function (text) {
-                if (text) {
-                    var component = templateToModel(text, name, url);
-                    component.created = function () {
-                        addStyles(name, component.style);
-                    };
-                    component.unmounted = function () {
-                        removeStyles(name);
-                    };
-                    resolve(component);
-                }
-            });
-        })));
+        instance.component(
+            name,
+            exports.defineAsyncComponent(
+                () =>
+                    new Promise((resolve, reject) => {
+                        fetch(url)
+                            .then(function (response) {
+                                return response.text();
+                            })
+                            .then(function (text) {
+                                if (text) {
+                                    templateToModel(text, name, url, function (
+                                        component
+                                    ) {
+                                        component.created = function () {
+                                            addStyles(name, component.style);
+                                        };
+                                        component.unmounted = function () {
+                                            removeStyles(name);
+                                        };
+                                        resolve(component);
+                                    });
+                                }
+                            });
+                    })
+            )
+        );
     }
     function patchComponent(instance, name, fun) {
         var result = fun();
-        if (!result || typeof (result) === 'string') {
-            var url = exports.basePath + exports.componentsPath + '/' + name.replaceAll('-', "/") + exports.componentExt;
+        if (!result || typeof result === "string") {
+            var url =
+                exports.basePath +
+                exports.componentsPath +
+                "/" +
+                name.replaceAll("-", "/") +
+                exports.componentExt;
             addComponent(instance, name, url);
             result = fun();
         }
@@ -108,37 +132,50 @@ var MyVueExt = (function () {
     }
     function configRouter(store, router) {
         if (router.options.history.base) {
-            exports.basePath = basePath = document.location.protocol + '//' + document.location.host + router.options.history.base
+            exports.basePath = basePath =
+                document.location.protocol +
+                "//" +
+                document.location.host +
+                router.options.history.base;
         }
         router.beforeEach((to, from, next) => {
-            var path = to.path === '/' ? exports.routerHome : to.path;
-            var name = path.substring(1).replaceAll('/', "-");
-            var url = exports.basePath + exports.componentsPath + exports.viewsPath + path + exports.componentExt;
+            var path = to.path === "/" ? exports.routerHome : to.path;
+            var name = path.substring(1).replaceAll("/", "-");
+            var url =
+                exports.basePath +
+                exports.componentsPath +
+                exports.viewsPath +
+                path +
+                exports.componentExt;
             if (!router.hasRoute(name)) {
-                fetch(url).then(function (response) {
-                    return response.text();
-                }).then(function (text) {
-                    if (text) {
-                        var model = templateToModel(text, name, url);
-                        var route = {
-                            name: name,
-                            path: to.path,
-                            component: model,
-                            meta: model.meta
-                        };
-                        router.addRoute(route);
-                        router.push({ path: to.path, query: to.query });
-                    }
-                });
-            }
-            else {
+                fetch(url)
+                    .then(function (response) {
+                        return response.text();
+                    })
+                    .then(function (text) {
+                        if (text) {
+                            templateToModel(text, name, url, function (component) {
+                                var route = {
+                                    name: name,
+                                    path: to.path,
+                                    component: component,
+                                    meta: component.meta,
+                                };
+                                router.addRoute(route);
+                                router.push({ path: to.path, query: to.query });
+                            });
+                        }
+                    });
+            } else {
                 next();
             }
         });
         router.beforeResolve(async function (to) {
-            var route = router.getRoutes().find(function (item, index) { return item.name === to.name });
-            if (route.components.default.style && !document.querySelector('head style.' + to.name)) {
-                append(document.head, route.components.default.style)
+            var route = router.getRoutes().find(function (item, index) {
+                return item.name === to.name;
+            });
+            if (route.components.default.style && !document.querySelector("head style." + to.name)) {
+                append(document.head, route.components.default.style);
             }
         });
         router.afterEach(function (to, from) {
@@ -149,9 +186,15 @@ var MyVueExt = (function () {
             }
         });
     }
-
+    function evalByImportUriData(script, url, func) {
+        script = script.replaceAll('./',exports.basePath + '/');
+        const dataUri = "data:text/javascript;charset=utf-8," + encodeURIComponent(script + '\n//@ sourceURL=' + url);
+        import(dataUri).then((namespaceObject) => {
+            func(namespaceObject.default ?? {});
+        });
+    }
     exports.addComponent = addComponent;
     exports.patchComponent = patchComponent;
     exports.configRouter = configRouter;
     return exports;
-}());
+})();
--- src/wwwroot/main.js
+++ src/wwwroot/main.js
@@ -3,34 +3,6 @@ import { createStore } from "vuex";
 import { createRouter, createWebHistory } from "vue-router";
 import ElementPlus from "element-plus";
 
-import * as Vue from "vue";
-import { loadModule } from "vue3-sfc-loader";
-
-
-const options = {
-    moduleCache: {
-        vue: Vue
-    },
-    async getFile(url) {
-
-        const res = await fetch(url);
-        if (!res.ok)
-            throw Object.assign(new Error(res.statusText + ' ' + url), { res });
-        return {
-            getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
-        }
-    },
-    addStyle(textContent) {
-        const style = Object.assign(document.createElement('style'), { textContent });
-        const ref = document.head.getElementsByTagName('style')[0] || null;
-        document.head.insertBefore(style, ref);
-    },
-}
-
-MyVueExt.load = function(url){
-    return defineAsyncComponent( () => loadModule(url, options));
-}
-
 //vue
 const app = createApp({
     data() {
